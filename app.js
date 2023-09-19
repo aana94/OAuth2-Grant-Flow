26dae59cd843060f0d344f0088bfa84a69309269
aana94 <aanchalgrover1994@gmail.com>
2023-09-19 00:08:38 -0400

    final fix app.js

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ const app = express();
 const dotenv = require('dotenv').config();
 const session = require('express-session');
 const passport = require('passport');
-var userProfile
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+const bodyParser = require("body-parser");
+const User = require('./models/User');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var LocalStorage = require('node-localstorage').LocalStorage;
+var userProfile, token
 
 const port = process.env.port || 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+
 app.use(session({
     resave: false,
     saveUninitialized: true,
-    secret: 'SECRET' 
+    secret: 'SECRET'
 }));
 
 app.get('/', (req, res) => {
-    // res.send('Hello Ohhoo!')
-    console.log('to the pages');
     res.render('index')
 })
 
@@ -29,10 +35,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/success', (req, res) => { res.render('success', {user: userProfile})});
-app.get('/error', (req, res) => res.send("Something went wrong!"));
+app.get('/error', (req, res) => {res.send("Something went wrong!")});
 
 passport.serializeUser((user, cb) => {
-cb(null, user);
+    cb(null, user);
 });
 
 passport.deserializeUser((obj, cb) => {
@@ -43,21 +49,36 @@ const authConfig = {
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.REDIRECT_URI,
+    scope : ['profile','email']
 }
 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy(authConfig,
-    function(accessToken, refreshToken, profile, done) {
-        console.log('in google strategy');
-        userProfile = profile
-        console.log('user detail', accessToken)
-        return done(null, userProfile)
+    async (accessToken, refreshToken, profile, done) => {
+        // console.log(profile);
+        let newUser = {
+            displayName: profile.displayName,
+            email: profile.emails[0].value
+        }
+        localStorage = new LocalStorage('./tokens');
+        localStorage.setItem('access-token', accessToken);
+        let user = await User.findOne({ displayName: profile.displayName });
+        // check if user is available in the database
+        // console.log(`if ${user}`)
+        if(user){
+            // if user is available
+            userProfile = profile
+            return done(null, userProfile)
+        } else {
+            // if user is not available create an entry
+            user = await User.create(newUser)
+            userProfile = profile
+            return done(null, userProfile)
+        }
     }
 ));
 
-// 'https://www.googleapis.com/auth/plus.login'
 
-app.get('/auth/google', passport.authenticate('google', { scope : ['profile','email'] }));
+app.get('/auth/google', passport.authenticate('google'));
 
 app.get('/auth/google/callback', passport.authenticate('google', { 
     failureRedirect: '/error',
